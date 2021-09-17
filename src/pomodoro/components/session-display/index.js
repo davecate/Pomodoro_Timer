eafe9a83273e031ae99284bf866c607ee0e8dbda
davecate <86169488+davecate@users.noreply.github.com>
2021-09-17 10:07:26 -0500

    Comment typo

--- src/pomodoro/components/session-display/index.js
+++ src/pomodoro/components/session-display/index.js
@@ -6,7 +6,7 @@ import SessionTitle from "./session-title";
 const SessionDisplay = ( { session, focusDuration, breakDuration } ) => {
     
     // NOTE FOR THINKFUL GRADERS/STUDENTS: Thinkful provides starter code 
-    // using session?.timeRemaining, but requires code to pass in Qualified's
+    // using session?.timeRemaining, but requires code to pass in Qualified,
     // which doesn't support using the new syntax inside of JSX.
 
     // Get remaining time from session object, using old syntax 
